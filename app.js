60cd4b28917c830b2602a245e1d5830aa98294cd
Mangesh <mdpatankar@gmail.com>
2019-10-22 00:32:30 +0530

    done 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello Welcome to IBN Cloud Pack!");
+  res.send("Hello Welcome to IBN Cloud Pack - part 2!");
 });
  
 module.exports.app = app;
